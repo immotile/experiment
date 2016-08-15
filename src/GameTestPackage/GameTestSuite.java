3554b2259cc58e2fb4f40c78fa3b05b1587a5800
root <root@localhost.localdomain>
2016-08-15 13:28:30 +0200

    Initial commit

---
+++ src/GameTestPackage/GameTestSuite.java
@@ -0,0 +1,13 @@
+package GameTestPackage;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTestSuite {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
